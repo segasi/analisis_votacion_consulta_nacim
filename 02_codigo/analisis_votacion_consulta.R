47358dc878eb1a86d7bf676646824ff27359b486
segasi <segasi@gmail.com>
2018-10-31 22:08:02 -0600

    Definir tema de gráficas

--- 02_codigo/analisis_votacion_consulta.R
+++ 02_codigo/analisis_votacion_consulta.R
@@ -4,3 +4,18 @@ p_load(scales, tidyverse)
 
 ### Cargar datos ----
 bd <- read_csv("01_datos/resultado_mesas20181029-1830.csv")
+
+### Definir tema de gráficas ----
+tema <-  theme_minimal() +
+  theme(text = element_text(family="Didact Gothic Regular", color = "grey35"),
+        plot.title = element_text(size = 24, face = "bold", margin = margin(10,0,20,0), family="Trebuchet MS Bold", color = "grey25"),
+        plot.subtitle = element_text(size = 16, face = "bold", colour = "#666666", margin = margin(0, 0, 20, 0), family="Didact Gothic Regular"),
+        plot.caption = element_text(hjust = 0, size = 15),
+        panel.grid = element_line(linetype = 2), 
+        panel.grid.minor = element_blank(),
+        legend.position = "bottom",
+        legend.title = element_text(size = 16, face = "bold", family="Trebuchet MS Bold"),
+        legend.text = element_text(size = 14, family="Didact Gothic Regular"),
+        legend.title.align = 0.5,
+        axis.title = element_text(size = 18, hjust = 1, face = "bold", margin = margin(0,0,0,0), family="Didact Gothic Regular"),
+        axis.text = element_text(size = 16, face = "bold", family="Didact Gothic Regular"))
